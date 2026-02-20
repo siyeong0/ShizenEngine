c30d8ab02895e3764b5fa4df9efce112ff33afbb
siyeong <sy1211w@gmail.com>
2026-02-20 11:45:55 +0900

    REF:

--- Engine/RuntimeData/Private/Material.cpp
+++ Engine/RuntimeData/Private/Material.cpp
@@ -372,7 +372,7 @@ namespace shz
 			{
 				gpDesc.RasterizerDesc.DepthBias = 2500;
 				gpDesc.RasterizerDesc.DepthBiasClamp = 0.0f;
-				gpDesc.RasterizerDesc.SlopeScaledDepthBias = 8.0f;
+				gpDesc.RasterizerDesc.SlopeScaledDepthBias = 16.0f;
 			}
 		}
 
